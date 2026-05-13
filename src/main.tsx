fc8feff58a858a8f89d895ab7e820795d9c1a0d5
Imtiyaj Bin Islam <imtiyajbinislam006@gmail.com>
2026-05-13 15:26:18 +0600

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+console.log("Starting React app...");
+
+const rootElement = document.getElementById("root");
+console.log("Root element:", rootElement);
+
+if (!rootElement) {
+  console.error("Root element not found!");
+} else {
+  console.log("Creating React root...");
+  const root = ReactDOM.createRoot(rootElement);
+  console.log("Rendering app...");
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("App rendered successfully!");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
